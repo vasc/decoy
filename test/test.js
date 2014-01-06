66a1692de05662693f530b92ac560f361ee6176c
Vasco Fernandes <vasco.box@gmail.com>
2014-01-06 10:53:21 +0100

    implementation of simple function and callback mocks

--- test/test.js
+++ test/test.js
@@ -6,11 +6,32 @@ chai.should();
 describe('decoy', function(){
 	var decoy = require('../lib/decoy');
 
-	describe('#module', function(){
-		var mock = decoy.module('randomMethod');
+	describe('#callback', function(){
+		var mockCallbackFunction = decoy.callback('argument');
 
-		it('when requested for an undefined variable it should return a mock function', function(){
-			mock.randomMethod.should.be.a('function');
-		})
+		it('should execute a callback sent as a last argument', function(testCb){
+			mockCallbackFunction('smth', function(){
+				testCb();
+			});
+		});
+
+		it('should execute a callback with the given argument', function(testCb){
+			mockCallbackFunction(function(arg){
+				arg.should.equal('argument');
+				testCb();
+			});
+		});
+
+		it('should throw an error if no callback is given', function(){
+			mockCallbackFunction.should.throw('No callback function passed');
+		});
+	});
+
+	describe('#function', function(){
+		var func = decoy.function('argument');
+
+		it('should return "argument" when called', function(){
+			func().should.be.eql('argument');
+		});
 	});
 });
